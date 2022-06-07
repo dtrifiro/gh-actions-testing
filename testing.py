311a06f6e218d9c6838aee0994dfd53b80755a60
Daniele Trifirò <daniele@iterative.ai>
2022-06-07 10:07:19 +0200

    windows testing

--- testing.py
+++ testing.py
@@ -1,2 +1,54 @@
+import os
+import subprocess
+import tempfile
+
+
 def main():
-    print("hello")
+    path = os.path.join(tempfile.gettempdir(), "dulwich-git-credential-store-test")
+
+    for shell in [True, False]:
+        print(f"Running {shell=}")
+        cmd = f"git credential-store --file {path} store"
+        if not shell:
+            cmd = cmd.split(" ")
+
+        url = "https://github.com"
+        username = "username"
+        password = "password"
+        subprocess_in = f"url={url}\nusername={username}\npassword={password}\n".encode(
+            "UTF-8"
+        )
+
+        try:
+            p = subprocess.run(
+                cmd, shell=shell, capture_output=True, check=True, input=subprocess_in
+            )
+            print(f"{p}")
+            print(f"{p.stdout=}")
+            print(f"{p.stderr=}")
+
+        except subprocess.CalledProcessError as exc:
+            print(f"Failed to run: {exc}")
+            print(f"{exc.stdout=}")
+            print(f"{exc.stderr=}")
+
+        subprocess_in = f"url={url}\n".encode("UTF-8")
+        cmd = f"git credential-store --file {path} get"
+        if not shell:
+            cmd = cmd.split(" ")
+        try:
+            p = subprocess.run(
+                cmd, shell=shell, capture_output=True, check=True, input=subprocess_in
+            )
+            print(f"{p}")
+            print(f"{p.stdout=}")
+            print(f"{p.stderr=}")
+
+        except subprocess.CalledProcessError as exc:
+            print(f"Failed to run: {exc}")
+            print(f"{exc.stdout=}")
+            print(f"{exc.stderr=}")
+
+
+if __name__ == "__main__":
+    main()
